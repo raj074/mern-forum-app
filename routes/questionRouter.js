98e59c821e9f183839069ef43f375a76ced616e2
raj074 <72184791+raj074@users.noreply.github.com>
2021-07-30 11:40:43 +0530

    Add files via upload

---
+++ routes/questionRouter.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const questionCtrl = require("../controllers/questionCtrl");
+const auth = require("../middleware/auth");
+
+router
+  .route("/questions")
+  .post(auth, questionCtrl.createQuestion)
+  .get(auth, questionCtrl.getActiveQuestions);
+
+  router.get("/questions/bountied", auth, questionCtrl.getBountiedQuestions);
+  router.get("/questions/all", auth, questionCtrl.getAllQuestions);
+  router.get("/questions/everyTags", auth, questionCtrl.getQuestionsFromEveryTags);
+
+router
+  .route("/questions/:id")
+  .get(auth, questionCtrl.getQuestion);
+
+  router.patch("/question/:id/upvote", auth, questionCtrl.upVoteQuestion);
+  router.patch("/question/:id/removeVote", auth, questionCtrl.removeVote);
+  router.patch("/question/:id/close", auth, questionCtrl.closeQuestion);
+  router.post("/question/:id/answer", auth, questionCtrl.answerQuestion);
+  router.post("/question/:id/bounty", auth, questionCtrl.putBounty);
+  router.patch("/question/:id/report", auth, questionCtrl.reportQuestion);
+
+
+
+module.exports = router;
